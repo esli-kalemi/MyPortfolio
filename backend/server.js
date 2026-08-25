7e97ae685d29874ec5a607310428461c38e8cbeb
Esli Kalemi <esli-kalemi>
2026-08-25 16:29:48 +0200

    Add contact form backend

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = 3000;
+
+
+// Middleware
+
+app.use(cors());
+
+app.use(express.json());
+
+
+// Test route
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Portfolio backend is running!"
+    });
+});
+
+
+// Start server
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
